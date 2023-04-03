2b670a1b5e029ba1d9e310e0c4bd610bd1769d14
2587877008@qq.com <2587877008@qq.com>
2023-04-03 22:21:35 +0800

    add analystic

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let BASE_URL = '/api'
+let BASE_URL = ''
 // BASE_URL=https://dev.pandafeeder.xyz
 
 const OPENAI_API_KEY = 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // replace with your API key
