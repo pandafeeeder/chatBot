217a74bd8c46e655f7eff58b667fd1c08b0ffc91
2587877008@qq.com <2587877008@qq.com>
2023-04-04 08:07:13 +0800

    change style

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import Markdown from 'vue3-markdown-it';
-import 'highlight.js/styles/monokai.css';
+import 'highlight.js/styles/github.css';
 import './index.scss'
 
 
